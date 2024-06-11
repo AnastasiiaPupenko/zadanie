c990d8b3181bc0603570f3407cc5103289b1024d
AnastasiiaPupenko <168479231+AnastasiiaPupenko@users.noreply.github.com>
2024-06-11 12:38:20 +0200

    Add files via upload

---
+++ 9.9.1(2).cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef struct {
+    int rows;
+    int cols;
+    int **data;
+} MAT;
+
+// Vytvoríme funkciu na inicializáciu štvorcovej matice, ktorá ju naplní nulami:
+
+MAT* mat_init(int rows, int cols) {
+    MAT *mat = (MAT *) malloc(sizeof(MAT));
+    mat->rows = rows;
+    mat->cols = cols;
+    mat->data = (int **)malloc(rows * sizeof(int *));
+    for(int i = 0; i < rows; i++) {
+        mat->data[i] = (int *)malloc(cols * sizeof(int));
+        for(int j = 0; j < cols; j++) {
+            mat->data[i][j] = 0;
+        }
+    }
+    return mat;
+}
+//Nezabudneme vytvoriť funkciu na uvoľnenie pamäte alokovanej pre maticu:
+
+void mat_free(MAT *mat) {
+    for(int i = 0; i < mat->rows; i++) {
+        free(mat->data[i]);
+    }
+    free(mat->data);
+    free(mat);
+}
+
+
+
+
+
+
+
+
+
